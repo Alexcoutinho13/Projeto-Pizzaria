97fc71f9fac003fbdbcaab55ed07cad1608bf552
Alexcoutinhoaraujo <alex.coutinho.araujo@gmail.com>
2024-09-11 21:36:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let modalQt = 1
+let cart = []
+let modalKey = 0
 const c = (el) => {
     return document.querySelector(el)
 }
@@ -16,9 +18,13 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--img img').setAttribute('src', item.img)
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault()
-        modalQt = 1
 
         let key = e.target.closest('.pizza-item').getAttribute('data-key')
+        modalQt = 1
+        modalKey = key
+
+
+
         c('.pizzaBig img').setAttribute('src', `${pizzaJson[key].img}`)
         c('.pizzaInfo h1').innerHTML = `${pizzaJson[key].name}`
         c('.pizzaInfo--actualPrice').innerHTML = `${pizzaJson[key].price.toFixed(2)}`
@@ -59,4 +65,39 @@ function closeModal() {
 }
   cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=> {
     item.addEventListener('click', closeModal)
+  })
+  c('.pizzaInfo--qtmais').addEventListener('click', ()=> {
+    c('.pizzaInfo--qt').innerHTML = modalQt += 1
+  })
+  c('.pizzaInfo--qtmenos').addEventListener('click', ()=> {
+    if(modalQt > 1) {
+        c('.pizzaInfo--qt').innerHTML = modalQt -= 1
+    }
+
+  })
+
+  cs('.pizzaInfo--size').forEach((size, sizeIndex)=> {
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+        size.classList.add('selected')
+    })
+  })
+    c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = c('.pizzaInfo--size.selected').getAttribute('data-key')
+
+    let identifier = pizzaJson[modalKey].id+'@'+size
+    let key = cart.findIndex((item) =>item.identifier==identifier)
+        if(key > -1) {
+            cart[key].qt += modalQt
+        } else {
+            cart.push({
+                identifier,
+                id:pizzaJson[modalKey].id,
+                size,
+                qt:modalQt
+            })
+    }
+
+    console.log(cart)
+    closeModal()
   })
